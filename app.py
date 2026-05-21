67f9fddfc503e03087629258eb215cce7fd83144
Ale0318 <alejafonsecas0318@gmail.com>
2026-05-20 22:39:07 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Serena",
+    page_icon="🫶",
+    layout="centered"
+)
+
+st.title("SERENA")
+
+st.subheader("Aplicación multimodal para acompañamiento emocional")
+
+st.write("Hola, estoy aquí contigo.")
