cfeec8b8a50cd0b49103e349c77109bf72e8aeb0
Ale0318 <alejafonsecas0318@gmail.com>
2026-05-26 14:26:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ with col2:
         justify-content:center;
         height:300px;
         font-size:120px;
-    ">🌸</div>
+    ">Logo.png</div>
     """, unsafe_allow_html=True)
 
 st.markdown("---")
